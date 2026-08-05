8814169a1d86c1a92db51a1d2d5c34224cb1656d
Snehankur Mondal <snehankur.mondal02@gmail.com>
2026-08-05 13:42:10 +0530

    Create brute-force.py

---
+++ Longest-Palindrome/brute-force.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        longest = ""
+
+        for i in range(len(s)):
+            for j in range(i, len(s)):
+                substring = s[i:j + 1]
+
+                if substring == substring[::-1]:
+                    if len(substring) > len(longest):
+                        longest = substring
+
+        return longest
